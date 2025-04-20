2389ac96e9b227a13fa402540374c32296a179cd
Enrique-OSM <enriquesalazar@MacBook-Air-de-Enrique.local>
2025-04-19 20:19:34 -0600

    primer commit de mis lugares

---
+++ app/src/main/java/com/example/mislugares/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mislugares;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
