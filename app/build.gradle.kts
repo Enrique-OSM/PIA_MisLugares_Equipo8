f0873ebb6b21251a6407072a37bc23d0c4e4d6c5
Enrique-OSM <enriquesalazar@MacBook-Air-de-Enrique.local>
2025-05-05 21:01:20 -0600

    dejamos todo listo para la interaccion con base de datos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
